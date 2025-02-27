456d64ff6d4430e318aff77d2d5892cd8a9a896e
Gilbert Osborne <gilbert.osborne@gmail.com>
2025-02-27 14:01:40 -0800

    Update nav.js

--- nav.js
+++ nav.js
@@ -1,5 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
-    fetch("nav.html")
+    // Determine the correct relative path to `nav.html`
+    let navPath = window.location.pathname.includes("/tools/") ? "../nav.html" : "nav.html";
+
+    fetch(navPath)
         .then(response => response.text())
         .then(data => {
             document.getElementById("nav-container").innerHTML = data;
